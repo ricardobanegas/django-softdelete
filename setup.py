70b1dde6bc79ae15101ee25ddac95e486c966596
Marek Waluś <marekwalus@gmail.com>
2018-07-10 14:29:11 +0200

    support for non softdelete objects from one to one relations

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='django-softdelete-kourts',
-      version='0.8.5',
+      version='0.8.6',
       description='Soft delete support for Django ORM, with undelete and many more!',
       author='Kourts',
       author_email='support@kourts.com',
